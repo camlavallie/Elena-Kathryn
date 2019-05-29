d9338b5fdecca68709beab577f361033afe4dd8b
Cameron LaVallie <camlavallie@gmail.com>
2019-05-29 17:49:53 -0500

    css fixes for mobile

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 class Home extends Component {
   render() {
