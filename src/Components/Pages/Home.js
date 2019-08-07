1fbedd7f89914cd9c4f27ae5a7b5366eb574e383
Cameron LaVallie <camlavallie@gmail.com>
2019-08-07 01:21:09 -0500

    make it better

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
       <Jumbotron className="jumbo-main">
           <h1>Hello beautiful people!</h1>
           <p className="lead">
-            “Once your life was full of sin’ s darkness, but now you have the very light of our Lord shining through you because of your union with him. Your mission is to live as children flooded with his revelation - light!”<br/>‭‭Ephesians‬‭ 5: 8
+            “Once your life was full of sin’s darkness, but now you have the very light of our Lord shining through you because of your union with him. Your mission is to live as children flooded with his revelation - light!”<br/>‭‭Ephesians‬‭ 5: 8
           </p>
        </Jumbotron>
        
