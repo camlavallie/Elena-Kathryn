4365c1d5ed0fb3dfb9d0a3efdfa01bf4c68ede01
Cameron LaVallie <camlavallie@gmail.com>
2019-08-16 09:31:19 -0500

    fixes

--- src/Components/Pages/photos.js
+++ src/Components/Pages/photos.js
@@ -1,17 +1,5 @@
 export const photos = [
   
-   {
-     src: "https://source.unsplash.com/2ShvY8Lf6l0/1600x1200",
-     srcSet: [
-       "https://source.unsplash.com/2ShvY8Lf6l0/500x375 500w",
-       "https://source.unsplash.com/2ShvY8Lf6l0/800x600 800w",
-       "https://source.unsplash.com/2ShvY8Lf6l0/1024x768 1024w",
-       "https://source.unsplash.com/2ShvY8Lf6l0/1600x1200 1600w"
-     ],
-     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-     width: 4,
-     height: 3
-   },
   {
 
     src: require('../../img/elephant.jpg'), 
