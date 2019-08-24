a63c17f2b1f2751c41fdd9243d117361b4d50e59
Cameron LaVallie <camlavallie@gmail.com>
2019-08-24 10:28:34 -0500

    hot fix

--- src/Components/Pages/photos.js
+++ src/Components/Pages/photos.js
@@ -1,83 +1,10 @@
 export const photos = [
   
-  {
-
-    src: require('../../img/elephant.jpg'), 
-    width: 600,
-    height: 800
-  },
   
   {
     src: require('../../img/bear.jpg'),
-     width: 600,
-       height: 800
+     width: 1,
+       height: 1
   },
-  {
-    src: require('../../img/lake.jpg'), 
-    width: 600,
-      height: 800
-  },
-  {
-    src: require('../../img/cactus.jpg'),
-    width: 600,
-      height: 800
-  },
-  {
-    src: require('../../img/dream-catcher.jpg'),
-   width: 600,
-    height:80
-  },
-  {
-    src: require('../../img/parrot.jpg'),
-    width: 600,
-    height: 800
-  },
-  {
-    src: require('../../img/zebra.jpg'),
-   width: 600,
-    height:80
-  },
-  {
-    src: require('../../img/giraffe.jpg'),
-    width: 600,
-    height: 800
-  },
-  {
-
-    src: require('../../img/elephant.jpg'),
-   width: 600,
-    height:80
-
-  }, {
-    src: require('../../img/bear.jpg'),
-    width: 600,
-    height: 800
-  }, {
-    src: require('../../img/lake.jpg'),
-    width: 600,
-    height: 800
-  }, {
-    src: require('../../img/cactus.jpg'),
-    width: 600,
-    height: 800
-  }, {
-    src: require('../../img/dream-catcher.jpg'),
-    width: 600,
-    height: 800
-  }, {
-    src: require('../../img/parrot.jpg'),
-   width: 600,
-    height:80
-  }, {
-    src: require('../../img/zebra.jpg'),
-   width: 600,
-    height: 80
-  }, 
-  {
-    src: require('../../img/giraffe.jpg'),
-    width: 600,
-    height: 800
-  },
-  
 ];
 export default photos;
